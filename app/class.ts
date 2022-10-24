f9488266a4410cb59ffa150b0fd75f90ea4a2672
Loic Robin <loic.robin@epsi.fr>
2022-10-24 10:09:12 +0200

    creation premieres classes

---
+++ app/class.ts
@@ -0,0 +1,20 @@
+class Shop{
+    name : string;
+
+    constructor(name: string){
+        this.name = name;
+    }
+}
+
+class Product{
+    name : string;
+    quality : number;
+    sellIn : number;
+
+    constructor(name: string, quality: number, sellIn: number){
+        this.name = name;
+        this.quality = quality;
+        this.sellIn = sellIn;
+    }
+}
+
---
+++ app/main.ts
@@ -0,0 +1,3 @@
+const LeeroyShop = new Shop("Leeroy Shop");
+const Sulfuras = new Product("Sulfuras", 80, 12);
+
